e79ad7775cbdbd7f1be3eb5ba521a82311c2d440
withobject <contact@withobject.design>
2025-08-19 08:30:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -124,7 +124,7 @@
   const projectImages = [
     [
       'Assets/cd_player_frame1_v2.png',
-      'https://i.pinimg.com/1200x/db/6e/55/db6e553509254016b399fb3ed17f1d13.jpg',
+      'Assets/wo_animation_frame5_v1.MP4',
       'https://i.pinimg.com/736x/ec/32/df/ec32dfd758ebf7cdad50d633638d9410.jpg',
       'https://i.pinimg.com/1200x/f9/48/bb/f948bb6a474d73b73b7b24ef0fa10234.jpg',
       'https://i.pinimg.com/1200x/8a/1d/8f/8a1d8f22816b78b3436566aeba72d438.jpg',
